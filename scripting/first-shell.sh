aa32aeb0bc193de980c7f2b6decf51021def2d34
Bharadwaj <mi549625@gmail.com>
2024-08-11 23:13:52 +0530

    adding new scripting files

---
+++ scripting/first-shell.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+name="Bharadwaj"
+var=15
+
+export echo "my name is $name"
+echo "Today is $var"
---
+++ scripting/third-shell.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+export org="muzigal"
+mentor="me"
+
+echo "my organization is $muzigal"
+echo "my mentor is $mentor"
+
+echo "calling the other file..."
+./first-shell.sh 
